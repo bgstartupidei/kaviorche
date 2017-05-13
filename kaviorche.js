590d9dec5e956c361d214cb9f614f40de208a4e0
aquilax <aquilax@gmail.com>
2017-05-13 19:07:38 +0200

    Updates

--- kaviorche.js
+++ kaviorche.js
@@ -1,11 +1,89 @@
 var k;
 
+var patterns = [`0 0 0 0 0 1 0 1 1 0 1 0 0 0 0 0
+0 0 0 0 0 1 0 1 1 0 1 0 0 0 0 0
+0 0 3 0 0 1 1 1 1 1 1 0 0 3 0 0
+0 0 3 0 0 1 1 1 1 1 1 0 0 3 0 0
+0 0 3 0 0 0 1 1 1 1 0 0 0 3 0 0
+0 0 3 0 0 0 1 1 1 1 0 0 0 3 0 0
+0 0 3 0 0 0 0 2 2 0 0 0 0 3 0 0
+0 3 0 0 2 0 0 2 2 0 0 2 0 0 3 0
+0 3 0 0 2 2 0 2 2 0 2 2 0 0 3 0
+0 3 0 0 2 2 0 2 2 0 2 2 0 0 3 0
+0 3 0 0 0 2 0 2 2 0 2 0 0 0 3 0
+0 3 0 0 0 2 2 2 2 2 2 0 0 0 3 0
+0 3 0 0 0 0 2 2 2 2 0 0 0 0 3 0
+0 3 0 0 0 0 0 2 2 0 0 0 0 0 3 0
+0 0 0 0 0 0 0 2 2 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 2 2 0 0 0 0 0 0 0`,
+`0 0 1 1 0 0 0 0 0 0 0 1 2 2 2 0
+0 1 2 2 1 0 0 0 0 0 1 1 2 0 0 0
+0 0 0 1 1 0 0 0 0 1 1 2 2 2 2 0
+0 0 0 1 1 0 0 0 1 1 1 1 2 0 0 0
+0 0 1 1 0 0 0 1 1 1 1 1 0 0 0 0
+0 0 1 1 0 0 2 1 1 1 1 2 2 2 2 0
+0 1 1 1 0 0 2 1 1 1 1 1 2 0 0 0
+0 1 1 1 0 0 0 2 1 1 1 1 0 0 0 0
+0 1 1 1 1 2 0 0 2 1 1 2 2 2 2 0
+1 1 1 1 1 2 2 0 0 2 1 1 2 0 0 0
+2 1 1 1 1 1 2 2 0 0 1 1 1 0 0 0
+2 1 1 1 1 1 1 1 1 0 0 1 1 0 0 0
+0 2 1 1 1 1 1 1 1 1 0 1 1 0 0 0
+0 2 2 1 1 1 1 1 1 1 1 1 1 0 0 0
+0 0 2 2 1 1 1 1 1 1 1 1 0 0 0 0
+0 0 0 0 3 0 3 0 0 0 0 0 0 0 0 0`,
+`0 1 0 0 2 0 0 0 0 0 0 2 0 0 1 0
+1 1 0 0 2 2 0 0 0 0 2 2 0 0 1 1
+0 0 1 0 2 2 2 0 0 2 2 2 0 1 0 0
+0 0 0 1 0 2 2 0 0 2 2 0 1 0 0 0
+2 2 2 0 1 0 2 0 0 2 0 1 0 2 2 2
+0 2 2 2 0 1 0 0 0 0 1 0 2 2 2 0
+0 0 2 2 2 0 1 0 0 1 0 2 2 2 0 0
+0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0
+0 0 2 2 2 0 1 0 0 1 0 2 2 2 0 0
+0 2 2 2 0 1 0 0 0 0 1 0 2 2 2 0
+2 2 2 0 1 0 2 0 0 2 0 1 0 2 2 2
+0 0 0 1 0 2 2 0 0 2 2 0 1 0 0 0
+0 0 1 0 2 2 2 0 0 2 2 2 0 1 0 0
+1 1 0 0 2 2 0 0 0 0 2 2 0 0 1 1
+0 1 0 0 2 0 0 0 0 0 0 2 0 0 1 0`,
+`0 0 0 0 1 1 0 0 0 0 1 1 0 0 0 0
+0 0 0 1 1 1 1 0 0 1 1 1 1 0 0 0
+0 0 1 1 1 1 1 0 0 1 1 1 1 1 0 0
+0 1 1 1 1 1 1 0 0 1 1 1 1 1 1 0
+1 1 1 1 1 2 1 0 0 1 2 1 1 1 1 1
+1 1 1 1 2 2 1 0 0 1 2 2 1 1 1 1
+0 1 1 1 1 1 0 0 0 0 1 1 1 1 1 0
+0 0 0 0 0 0 0 3 3 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 3 3 0 0 0 0 0 0 0
+0 1 1 1 1 1 0 0 0 0 1 1 1 1 1 0
+1 1 1 1 2 2 1 0 0 1 2 2 1 1 1 1
+1 1 1 1 1 2 1 0 0 1 2 1 1 1 1 1
+0 1 1 1 1 1 1 0 0 1 1 1 1 1 1 0
+0 0 1 1 1 1 1 0 0 1 1 1 1 1 0 0
+0 0 0 1 1 1 1 0 0 1 1 1 1 0 0 0
+0 0 0 0 1 1 0 0 0 0 1 1 0 0 0 0`
+];
+
+
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('container');
-  // frameRate(20)
   k = new Kaviorche()
   k.init(windowWidth, windowHeight)
+  console.log(windowWidth, windowHeight);
+}
+
+function mousePressed() {
+  var fs = fullscreen();
+  fullscreen(!fs);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  k.init(windowWidth, windowHeight)
+  console.log(windowWidth, windowHeight);
 }
 
 function draw() {
@@ -16,9 +94,9 @@ function getColors() {
   return [
     '#933535',
     '#e2404b',
-    '#e2404b',
     '#5b97bb',
     '#f0d36f',
+    '#969952'
   ];
 }
 
@@ -69,77 +147,60 @@ function getSquares(rectangles, min) {
     var height = r[3] - r[1];
     if (width > height) {
       if (height >= min) {
-        var sq = [r[0] + Math.ceil((width-height) /2), r[1], height, height];
+        var sq = [r[0] + Math.ceil(0, (width-height)), r[1], height, height];
         squares.push(sq)
       }
       return;
     }
     if (width >= min) {
-      var sq = [r[0], r[1] + Math.ceil((height - width) / 2), width, width];
+      var sq = [r[0], r[1] + Math.ceil(0, (height - width)), width, width];
       squares.push(sq);
     }
   });
   return squares;
 }
 
-figure = `0 0 0 0 0 1 0 1 1 0 1 0 0 0 0 0
-0 0 0 0 0 1 0 1 1 0 1 0 0 0 0 0
-0 0 3 0 0 1 1 1 1 1 1 0 0 3 0 0
-0 0 3 0 0 1 1 1 1 1 1 0 0 3 0 0
-0 0 3 0 0 0 1 1 1 1 0 0 0 3 0 0
-0 0 3 0 0 0 1 1 1 1 0 0 0 3 0 0
-0 0 3 0 0 0 0 2 2 0 0 0 0 3 0 0
-0 3 0 0 2 0 0 2 2 0 0 2 0 0 3 0
-0 3 0 0 2 2 0 2 2 0 2 2 0 0 3 0
-0 3 0 0 2 2 0 2 2 0 2 2 0 0 3 0
-0 3 0 0 0 2 0 2 2 0 2 0 0 0 3 0
-0 3 0 0 0 2 2 2 2 2 2 0 0 0 3 0
-0 3 0 0 0 0 2 2 2 2 0 0 0 0 3 0
-0 3 0 0 0 0 0 2 2 0 0 0 0 0 3 0
-0 0 0 0 0 0 0 2 2 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 2 2 0 0 0 0 0 0 0`;
-
+function getPattern() {
+  return random(patterns).replace(/[^\d]/gmi, '');
+}
 
 function Kaviorche() {
+  this.min = 64;
+  this.minSquares = 3;
+  this.maxSquares = 10;
+  this.backgroundColor = '#fefff0';
   this.width;
   this.height;
-  this.min = 64;
   this.currentSquare = 0;
   this.figure;
 
   this.init = (width, height) => {
     this.width = width;
     this.height = height;
-    background('#fff');
-    this.rectangles = getRectangles(width, height, this.min, 20);
+    background(this.backgroundColor);
+    this.rectangles = getRectangles(width, height, this.min, Math.ceil(random(this.minSquares, this.maxSquares)));
     this.squares = shuffle(getSquares(this.rectangles, this.min));
     this.currentSquare = 0;
     this.figure = new Figure();
-    this.pattern = figure;
+    // this.pattern = getPattern();
     this.initFigure();
   }
 
   this.initFigure = () => {
     this.figure.init(
-      this.pattern,
+      getPattern(),
       this.squares[this.currentSquare][0],
       this.squares[this.currentSquare][1],
-      Math.ceil(this.squares[this.currentSquare][2]/16)
+      Math.floor(this.squares[this.currentSquare][2]/16)
     );
   }
 
   this.draw = () => {
-    // this.squares.forEach((el, n) => {
-    //   rect(...el);
-    //   text(n, el[0]+2, el[1]+10);
-    //   text(el, el[0]+2, el[1]+20);
-    // });
-
     if (!this.figure.done()) {
       this.figure.nextFrame();
     } else {
+      this.currentSquare++;
       if (this.currentSquare < this.squares.length) {
-        this.currentSquare++;
         this.initFigure()
       } else {
         this.init(this.width, this.height);
@@ -153,11 +214,20 @@ function Figure() {
 
   this.init = (pattern, x, y, size) => {
     this.pos = 0;
-    this.pattern = pattern.replace(/[^\d]/gmi, '');
+    this.pattern = pattern;
     this.x = x;
     this.y = y;
     this.size = size;
     this.colors = shuffle(getColors());
+    if (this.size < 10) {
+      this.strokeWeight = 1;
+    } else if (this.size < 20) {
+      this.strokeWeight = 2;
+    } else if (this.size < 30) {
+      this.strokeWeight = 3;
+    } else {
+      this.strokeWeight = 4;
+    }
   }
 
   this.nextFrame = () => {
@@ -165,6 +235,7 @@ function Figure() {
     y = Math.floor(this.pos / 16)
     clr = this.pattern[this.pos];
     if (this.pattern[this.pos] > 0) {
+      strokeWeight(this.strokeWeight);
       stroke(this.colors[clr-1])
       line(
         this.x + x*this.size,
@@ -178,15 +249,6 @@ function Figure() {
         this.x + x*this.size +this.size,
         this.y + y*this.size
       )
-      // line(x, y + len, x + len, y)
-
-      // fill(this.colors[clr-1]);
-      // rect(
-      //   this.x + x*this.size,
-      //   this.y + y*this.size,
-      //   this.size,
-      //   this.size
-      // );
     }
     this.pos += 1
   }
